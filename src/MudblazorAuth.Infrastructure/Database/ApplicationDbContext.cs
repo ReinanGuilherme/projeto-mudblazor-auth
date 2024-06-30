73069cb8341db6f17782b3563ae8a922ee1167b6
ReinanGuilherme <r.guilhermedeoliveira3dev@gmail.com>
2024-06-30 09:37:15 -0300

    corrigindo seeds e add migrations

--- src/MudblazorAuth.Infrastructure/Database/ApplicationDbContext.cs
+++ src/MudblazorAuth.Infrastructure/Database/ApplicationDbContext.cs
@@ -19,49 +19,6 @@ namespace MudblazorAuth.Infrastructure.Database
 		{
 			ConfigureTableAndColumnNamesLowercase(modelBuilder);
 
-			modelBuilder.Entity<Account>(entity =>
-			{
-				entity.ToTable("accounts");
-				entity.HasKey(e => e.Id).HasName("accounts_id_primary");
-				entity.Property(e => e.Username).IsRequired().HasColumnName("user_name").HasMaxLength(255);
-				entity.Property(e => e.Password).IsRequired().HasColumnName("password").HasMaxLength(255);
-				entity.HasOne(e => e.Profile)
-					  .WithMany(p => p.Accounts)
-					  .HasForeignKey(e => e.IdProfile)
-					  .HasConstraintName("accounts_profile_id_foreign");
-				// Ensure UserName is unique
-				entity.HasIndex(e => e.Username).IsUnique().HasDatabaseName("accounts_user_name_unique");
-			});
-
-			modelBuilder.Entity<Profile>(entity =>
-			{
-				entity.ToTable("profiles");
-				entity.HasKey(e => e.Id).HasName("profiles_id_primary");
-				entity.Property(e => e.Description).IsRequired().HasColumnName("description").HasMaxLength(255);
-			});
-
-			modelBuilder.Entity<Page>(entity =>
-			{
-				entity.ToTable("pages");
-				entity.HasKey(e => e.Id).HasName("pages_id_primary");
-				entity.Property(e => e.Url).IsRequired().HasColumnName("url").HasMaxLength(255);
-				entity.Property(e => e.IsPrivate).IsRequired().HasColumnName("is_authenticated");
-			});
-
-			modelBuilder.Entity<ProfilePage>(entity =>
-			{
-				entity.ToTable("profile_pages");
-				entity.HasKey(e => e.Id).HasName("profile_id_pages_primary");
-				entity.HasOne(e => e.Profile)
-					  .WithMany(p => p.ProfilePages)
-					  .HasForeignKey(e => e.IdProfile)
-					  .HasConstraintName("profile_pages_id_profile_foreign");
-				entity.HasOne(e => e.Page)
-					  .WithMany(p => p.ProfilePages)
-					  .HasForeignKey(e => e.IdPage)
-					  .HasConstraintName("profile_pages_page_id_foreign");
-			});
-
 			SeedData(modelBuilder); // Seed database with predefined information.
 		}
 
@@ -98,33 +55,33 @@ namespace MudblazorAuth.Infrastructure.Database
 		private void SeedData(ModelBuilder modelBuilder)
 		{
 			modelBuilder.Entity<Profile>().HasData(
-				new Profile { Description = "Admin" },
-				new Profile { Description = "User" }
+				new Profile { Id = 1, Description = "Admin" },
+				new Profile { Id = 2, Description = "User" }
 			);
 
 			modelBuilder.Entity<Page>().HasData(
-				new Page { Url = "/SignIn", IsPrivate = false },
-				new Page { Url = "/SignUp", IsPrivate = false },
+				new Page { Id = 1, Url = "/SignIn", IsPrivate = false },
+				new Page { Id = 2, Url = "/SignUp", IsPrivate = false },
 				//Acesso Admin e User
-				new Page { Url = "/Home", IsPrivate = true },
-				new Page { Url = "/Counter", IsPrivate = true },
-				new Page { Url = "/Weather", IsPrivate = true },
+				new Page { Id = 3, Url = "/Home", IsPrivate = true },
+				new Page { Id = 4, Url = "/Counter", IsPrivate = true },
+				new Page { Id = 5, Url = "/Weather", IsPrivate = true },
 				//Acessi Admnin
-				new Page { Url = "/ProfilePermissionsPage", IsPrivate = true },
-				new Page { Url = "/Users", IsPrivate = true },
-				new Page { Url = "/AccessDenied", IsPrivate = true }
+				new Page { Id = 6, Url = "/ProfilePermissionsPage", IsPrivate = true },
+				new Page { Id = 7, Url = "/Users", IsPrivate = true },
+				new Page { Id = 8, Url = "/AccessDenied", IsPrivate = true }
 			);
 
 			modelBuilder.Entity<ProfilePage>().HasData(
-				new ProfilePage { IdProfile = 1, IdPage = 3 },
-				new ProfilePage { IdProfile = 1, IdPage = 4 },
-				new ProfilePage { IdProfile = 1, IdPage = 5 },
-				new ProfilePage { IdProfile = 1, IdPage = 6 },
-				new ProfilePage { IdProfile = 1, IdPage = 7 },
-				new ProfilePage { IdProfile = 1, IdPage = 8 },
-				new ProfilePage { IdProfile = 2, IdPage = 3 },
-				new ProfilePage { IdProfile = 2, IdPage = 4 },
-				new ProfilePage { IdProfile = 2, IdPage = 5 }
+				new ProfilePage { Id = 1, IdProfile = 1, IdPage = 3 },
+				new ProfilePage { Id = 2, IdProfile = 1, IdPage = 4 },
+				new ProfilePage { Id = 3, IdProfile = 1, IdPage = 5 },
+				new ProfilePage { Id = 4, IdProfile = 1, IdPage = 6 },
+				new ProfilePage { Id = 5, IdProfile = 1, IdPage = 7 },
+				new ProfilePage { Id = 6, IdProfile = 1, IdPage = 8 },
+				new ProfilePage { Id = 7, IdProfile = 2, IdPage = 3 },
+				new ProfilePage { Id = 8, IdProfile = 2, IdPage = 4 },
+				new ProfilePage { Id = 9, IdProfile = 2, IdPage = 5 }
 			);
 		}
 	}
---
+++ src/MudblazorAuth.Infrastructure/Migrations/20240630123652_InigitalMigration.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MudblazorAuth.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InigitalMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "page",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    url = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    isprivate = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_page", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "profiles",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    description = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_profiles", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "accounts",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    idprofile = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_accounts", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_accounts_profiles_idprofile",
+                        column: x => x.idprofile,
+                        principalTable: "profiles",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "profilepages",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    idprofile = table.Column<long>(type: "bigint", nullable: false),
+                    idpage = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_profilepages", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_profilepages_page_idpage",
+                        column: x => x.idpage,
+                        principalTable: "page",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_profilepages_profiles_idprofile",
+                        column: x => x.idprofile,
+                        principalTable: "profiles",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "page",
+                columns: new[] { "id", "isprivate", "url" },
+                values: new object[,]
+                {
+                    { 1L, false, "/SignIn" },
+                    { 2L, false, "/SignUp" },
+                    { 3L, true, "/Home" },
+                    { 4L, true, "/Counter" },
+                    { 5L, true, "/Weather" },
+                    { 6L, true, "/ProfilePermissionsPage" },
+                    { 7L, true, "/Users" },
+                    { 8L, true, "/AccessDenied" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "profiles",
+                columns: new[] { "id", "description" },
+                values: new object[,]
+                {
+                    { 1L, "Admin" },
+                    { 2L, "User" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "profilepages",
+                columns: new[] { "id", "idpage", "idprofile" },
+                values: new object[,]
+                {
+                    { 1L, 3L, 1L },
+                    { 2L, 4L, 1L },
+                    { 3L, 5L, 1L },
+                    { 4L, 6L, 1L },
+                    { 5L, 7L, 1L },
+                    { 6L, 8L, 1L },
+                    { 7L, 3L, 2L },
+                    { 8L, 4L, 2L },
+                    { 9L, 5L, 2L }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_accounts_idprofile",
+                table: "accounts",
+                column: "idprofile");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_profilepages_idpage",
+                table: "profilepages",
+                column: "idpage");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_profilepages_idprofile",
+                table: "profilepages",
+                column: "idprofile");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "accounts");
+
+            migrationBuilder.DropTable(
+                name: "profilepages");
+
+            migrationBuilder.DropTable(
+                name: "page");
+
+            migrationBuilder.DropTable(
+                name: "profiles");
+        }
+    }
+}
